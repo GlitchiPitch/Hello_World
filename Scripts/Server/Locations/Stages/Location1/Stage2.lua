04e6c4d700bb59b8b1a91dba77bb55a2505fa722
GlitchiPitch <141921165+GlitchiPitch@users.noreply.github.com>
2023-09-10 17:09:54 +0500

    update

--- Scripts/Server/Locations/Stages/Location1/Stage2.lua
+++ Scripts/Server/Locations/Stages/Location1/Stage2.lua
@@ -5,16 +5,17 @@ local Stage = {}
 
 
 -- dont forget uncomment 38 line about spawn character
-
-local CLOCK_TIME = 14
-local AMBIENT = Color3.fromRGB(1,1,1)
+-- надо поиграться с камерой чтобы обзор был похож на 1д игру
+ 
+local CLOCK_TIME = 0
+local AMBIENT = Color3.fromRGB(10,10,10)
 -- local WALL_HEIGHT = 20
 -- local BARRIER_SIZE = 10
 local WALL_COLOR = Color3.new(1,1,1)
 local TRIGGER_COLOR = Color3.new(1,1,1)
 -- local ROOM_VOLUME = 100
 
-local ENEMY_COLOR = Color3.new(0.8, 0.5, 0.9)
+local MONSTER_COLOR = Color3.new(0.8, 0.5, 0.9)
 
 Stage.__index = Stage
 
@@ -41,7 +42,7 @@ function Stage:Init()
     self:Setup()
     
     for i = 1, 3 do -- level count
-        self:SpawnRoom(1)
+        self:SpawnRoom()
         if self.Game.Player.Character.HumanoidRootPart and self.PlayerSpawnPoint then
             self.Game.Player.Character.HumanoidRootPart.CFrame = self.PlayerSpawnPoint.CFrame
         end
@@ -73,13 +74,12 @@ function Stage:AddLightToCharacter()
     light.Parent = att
 end
 
-function Stage:SpawnRoom(levelIndex) -- called 3 times
+function Stage:SpawnRoom() -- called 3 times
     -- возможно переписать без удаления и создания новой комнаты, просто обращаться к меодельке конмнаты и именять позицию и размеры, можно еще это сделать с твином, 
     -- но контент надо удалять или тоже просто пермещать, посмотрим
-    -- на этом этапе добавить в игрока поинт лайт чтобы только в близи было видно стены
     local properties = {
         wallsPositionValue = 50 + (50 * .3),
-        ySize = 15 + ((15 * levelIndex) * .3)
+        wallHeight = 15 + (15 * .3)
     }
 
     local room = self:CreateRoom(properties)
@@ -89,7 +89,7 @@ end
 function Stage:CreateRoom(properties)
     local value = properties.wallsPositionValue
     local Y = 10
-    local ySize = properties.ySize
+    local wallHeight = properties.wallHeight
     local wallsPositions = {
                 {value, Y, 0},
                 {0, Y, value},
@@ -98,7 +98,7 @@ function Stage:CreateRoom(properties)
             }
     local model = Instance.new('Model')
     model.Parent = workspace
-    -- model:PivotTo(LocalizationService.CFrame)  здесь взять центр локации в трех векторах и спавнить по середине эту комнату.
+    -- model:PivotTo(Location.CFrame)  здесь взять центр локации в трех векторах и спавнить по середине эту комнату.
     for i = 1, 6 do
         local wall = Instance.new('Part')
         wall.Parent = model
@@ -107,7 +107,7 @@ function Stage:CreateRoom(properties)
         wall.Color = WALL_COLOR
         if wallsPositions[i] then 
             wall.Position = Vector3.new(table.unpack(wallsPositions[i]))
-            local x, y, z = wall.Position.X == 0 and math.abs(wall.Position.Z) * 2 or 1, ySize, wall.Position.Z == 0 and math.abs(wall.Position.X) * 2 or 1
+            local x, y, z = wall.Position.X == 0 and math.abs(wall.Position.Z) * 2 or 1, wallHeight, wall.Position.Z == 0 and math.abs(wall.Position.X) * 2 or 1
             wall.Size = Vector3.new(x, y, z)
         else
             local _, modelSize = model:GetBoundingBox()
@@ -116,8 +116,16 @@ function Stage:CreateRoom(properties)
         end
     end
 
-
     return model
+end
+
+function Stage:CreateMonster(properties)
+    print('CREATE MONSTER')
+    local monster = Instance.new('Part')
+    monster.Color = MONSTER_COLOR
+    monster.Size = Vector3.new(5, properties.wallHeight, 5)
+
+    return monster
 end
 
 function Stage:CreateContent(room, properties)
@@ -163,21 +171,12 @@ function Stage:CreateContent(room, properties)
 
         target.Material = Enum.Material.Neon
         target.Color = TRIGGER_COLOR
-        target.Size = Vector3.new(5, properties.ySize, 5)
-
-        -- local x = math.random(-properties.wallsPositionValue + target.Size.X * 1.5 , properties.wallsPositionValue - target.Size.X * 1.5)
-        -- local y = 10
-        -- local z = math.random(-properties.wallsPositionValue + target.Size.Z * 1.5, properties.wallsPositionValue - target.Size.Z * 1.5)
-
-        -- -- target.CFrame = room:GetPivot() * CFrame.new(x, y, z)
-
+        target.Size = Vector3.new(5, properties.wallHeight, 5)
         target.Touched:Connect(function(hitPart)
             if not game.Players:GetPlayerFromCharacter(hitPart.Parent) then return end
-
             self.Level += 1
             target:Destroy()
             room:Destroy()
-
         end)
     end
 
@@ -190,9 +189,8 @@ function Stage:CreateContent(room, properties)
 
     local function setupWalls(wall)
         wall.Color = WALL_COLOR
-        local size = {{wallSize - 2, properties.ySize, 1}, {1, properties.ySize, wallSize - 2}}
+        local size = {{wallSize - 2, properties.wallHeight, 1}, {1, properties.wallHeight, wallSize - 2}}
         wall.Size = Vector3.new(table.unpack(size[math.random(#size)]))
-        -- wall.Size = Vector3.new(5, properties.ySize, 5)
     end
     local function createPart(name)
         local part = Instance.new('Part')
@@ -202,7 +200,7 @@ function Stage:CreateContent(room, properties)
 
         if name == 'target' then
             if self.Level == 3 then
-                self:CreatePortal()
+                self:CreatePortal(setupTarget(part))
             else
                 setupTarget(part)
             end
@@ -214,29 +212,32 @@ function Stage:CreateContent(room, properties)
 
         return part
     end
-    local a = math.random(#nodes)
+    local a = math.random(1,10) --math.random(#nodes)
     local spawnIndex = a  
     a = math.random(#nodes / 2, #nodes)
-    local targetIndex = spawnIndex ~= a and a or  a - 1
+    local targetIndex = a -- spawnIndex ~= a and a or  a - 1
+
+    local monsterIndex = targetIndex - 1
 
     for i, pos in pairs(nodes) do
-        -- if math.random(2) == 1 then continue end
         local part
         if i == spawnIndex then
             part = createPart('spawn')
         elseif i == targetIndex then
             part = createPart('target')
+        elseif i == monsterIndex and self.Level == 3 then
+            local monster = self:CreateMonster(properties)
+            monster.Parent = room
+            part = monster
         else
             part = createPart('wall')
         end
         part.Position = pos
-        -- part.Name = i
-        -- wait(2)
     end
 end
 
-function Stage:CreatePortal()
-    
+function Stage:CreatePortal(part)
+    part.Color = Color3.new(0,1,0)
 end
 
 return Stage
