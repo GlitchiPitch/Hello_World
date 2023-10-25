e7f4fe1288bf0f2ce443c9030ba824689500dba6
GlitchiPitch <141921165+GlitchiPitch@users.noreply.github.com>
2023-10-25 14:00:59 +0500

    upd

--- Scripts/Server/Locations/Stages/Location3/Stage4.lua
+++ Scripts/Server/Locations/Stages/Location3/Stage4.lua
@@ -1,3 +1,4 @@
+local Lighting = game:GetService("Lighting")
 -- THIS IS FINAL STAGE --
 
 local function createPart(parent, position, size)
@@ -10,30 +11,45 @@ local function createPart(parent, position, size)
 	return part
 end
 
-local function createRoom(parent, roomProperties, isCorridor)
-	
-	local sides = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} }
+local function createRoom(parent, roomProperties, isCorridor, ...)
+	local sides = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } }
 	local model = parent
 
 	for i, side in pairs(sides) do
-		local pos = Vector3.new(side[1] * roomProperties.roomSize / 2, roomProperties.wallHeight / 2, side[2] * roomProperties.roomSize / (isCorridor and 1 or 2))
-		local size = Vector3.new(side[2] == 0 and 1 or math.abs(side[2] * roomProperties.roomSize), roomProperties.wallHeight, side[1] == 0 and 1 or math.abs(side[1] * roomProperties.roomSize) + (isCorridor and roomProperties.roomSize or 0))
+		local pos = Vector3.new(
+			side[1] * roomProperties.roomSize / 2,
+			roomProperties.wallHeight / 2,
+			side[2] * roomProperties.roomSize / (isCorridor and 1 or 2)
+		)
+		local size = Vector3.new(
+			side[2] == 0 and 1 or math.abs(side[2] * roomProperties.roomSize),
+			roomProperties.wallHeight,
+			side[1] == 0 and 1
+				or math.abs(side[1] * roomProperties.roomSize) + (isCorridor and roomProperties.roomSize or 0)
+		)
 		local wall = createPart(model, pos, size)
 		wall.Material = roomProperties.material
 		wall.Color = Color3.new(0.478431, 0.788235, 0.466666)
-		wall.Name = 'wall'
+		wall.Name = "wall"
+		if isCorridor and side[2] ~= 0 then
+			table.insert(..., wall)
+			wall.Size += Vector3.new(2, 0, 2)
+			wall.CanCollide = false
+		end
 	end
 
 	local pivot, roomSize = model:GetBoundingBox()
 
-	local roof = createPart(model, pivot.Position + Vector3.new(0, roomSize.Y / 2, 0), Vector3.new(roomSize.X, 1, roomSize.Z))
-	local bottom = createPart(model, pivot.Position + Vector3.new(0, -roomSize.Y / 2, 0), Vector3.new(roomSize.X, 1, roomSize.Z))
-	roof.Color, bottom.Color = Color3.new(.5,.5,.5), Color3.new(.5,.5,.5)
+	local roof =
+		createPart(model, pivot.Position + Vector3.new(0, roomSize.Y / 2, 0), Vector3.new(roomSize.X, 1, roomSize.Z))
+	local bottom =
+		createPart(model, pivot.Position + Vector3.new(0, -roomSize.Y / 2, 0), Vector3.new(roomSize.X, 1, roomSize.Z))
+	roof.Color, bottom.Color = Color3.new(0.5, 0.5, 0.5), Color3.new(0.5, 0.5, 0.5)
 	roof.Material, bottom.Material = roomProperties.material, roomProperties.material
-	roof.Name, bottom.Name = 'roof', 'bottom'
+	roof.Name, bottom.Name = "roof", "bottom"
 	roof.CanCollide = false
 
-	return model
+	return model, isCorridor and ...
 end
 
 local Stage = {}
@@ -53,57 +69,86 @@ function Stage.Create(game_)
 	return self
 end
 
-function Stage:Setup() 
-	self.Game.Player.Character:ScaleTo(.1)
+function Stage:Setup()
+	self.Game.Player.Character:ScaleTo(0.1)
+	local colorCorrection = Instance.new("ColorCorrectionEffect")
+	colorCorrection.Parent = Lighting
 end
 
 function Stage:CreateCorridor()
-	local corridorModel, blackRoom = Instance.new("Model"), Instance.new('Model')
+	local corridorModel, blackRoom = Instance.new("Model"), Instance.new("Model")
 	corridorModel.Parent, blackRoom.Parent = workspace, workspace
 
 	self.BlackRoom = createRoom(blackRoom, {
 		roomSize = 10,
 		wallHeight = 10,
-		material = Enum.Material.Rubber
+		material = Enum.Material.Rubber,
 	}, false)
-
 
 	blackRoom:PivotTo(self.SpawnPivot[1] * CFrame.new(0, -self.SpawnPivot[2].Y, 0)) --* CFrame.new(0, -50, 0)
 
-	self.Corridor = createRoom(corridorModel, {
+	self.Corridor, self.CorridorTeleports = createRoom(corridorModel, {
 		roomSize = 25,
 		wallHeight = 10,
-		material = Enum.Material.Rubber
-	}, true)
+		material = Enum.Material.Rubber,
+	}, true, {})
 
-	corridorModel:PivotTo(self.SpawnPivot[1] * CFrame.new(0, -self.SpawnPivot[2].Y,100)) -- * CFrame.new(0, -50, 100)
+	corridorModel:PivotTo(self.SpawnPivot[1] * CFrame.new(0, -self.SpawnPivot[2].Y, 100)) -- * CFrame.new(0, -50, 100)
+	self.CorridorTeleports[1].Material, self.CorridorTeleports[1].Color = Enum.Material.Neon, Color3.new(1, 1, 1)
+	self.CorridorTeleports[2].Material, self.CorridorTeleports[2].Color = Enum.Material.Neon, Color3.new(1, 1, 1)
 end
 
-function Stage:CreateCorridorContent(corridor)
-	
+function Stage:CreateCorridorContent(corridor, teleportWall, returnWall)
+	local teleports = {self.CorridorTeleports[1], self.CorridorTeleports[2]}
+	table.remove(teleports, table.find(teleports, teleportWall, 1))
+	local touchConnect
+	touchConnect = teleports[1].Touched:Connect(function(hitPart)
+		if self.Game.Player == game.Players:GetPlayerFromCharacter(hitPart.Parent) then
+			touchConnect:Disconnect()
+			game:GetService("TweenService")
+				:Create(
+					Lighting:FindFirstChild("ColorCorrection"),
+					TweenInfo.new(0.5, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut, 0, true),
+					{ Brightness = 3 }
+				)
+				:Play()
+			self.Game.Player.Character:MoveTo(returnWall.Position)
+			self:CreateBlackRoomContent()
+		end
+	end)
 end
 
 function Stage:CreateBlackRoomContent()
-	local cf, size = self.BlackRoom:GetBoundingBox()
 	local walls = {}
-	for _, obj in pairs(self.BlackRoom:GetChildren()) do if obj.Name == 'wall' then table.insert(walls, obj) end end
+	for _, obj in pairs(self.BlackRoom:GetChildren()) do
+		if obj.Name == "wall" then
+			table.insert(walls, obj)
+			obj.Color = Color3.new(0, 0, 0)
+			obj.CanCollide = true
+		end
+	end
 	local currentWall = walls[math.random(#walls)]
-	local pos = currentWall.Size.Z == 1 and (currentWall.CFrame.X < cf.X and currentWall.Position + Vector3.new(1,0,0) or currentWall.Position - Vector3.new(1,0,0)) or
-				(currentWall.CFrame.Z < cf.Z and currentWall.Position + Vector3.new(0,0,1) or currentWall.Position - Vector3.new(0,0,1))
-	local size = Vector3.new(1,1,1)
-	local targetSize = currentWall.Size
-	local portal = createPart(self.BlackRoom, pos, size)
-
-	local changeSizeConnect
-	local characterPos = self.Game.Player.Character:GetPivot().Position
-	changeSizeConnect = game:GetService('RunService').Heartbeat:Connect(function(deltaTime)
-		-- local charPos, portalPos
-		-- if math.abs(Vector3.new(charPos.X, 0, charPos.Z) - math.abs(portalPos).Magnitude) < 5 then
-		-- 	portal.Size = targetSize
-		-- else
-		local mag = (Vector3.new(characterPos.X, 0, characterPos.Z) - Vector3.new(portal.Position.X, 0, portal.Position.Z)).Magnitude
-		portal.Size *= Vector3.new(targetSize.X == 1 and 1 or mag / 10, mag / 10, targetSize.Z == 1 and 1 or mag / 10)
-		-- print((Vector3.new(characterPos.X, 0, characterPos.Z) - Vector3.new(portal.Position.X, 0, portal.Position.Z)).Magnitude)
+	currentWall.Material = Enum.Material.Neon
+	currentWall.Color = Color3.new(1, 1, 1)
+	local defaltSize = currentWall.Size
+	currentWall.Size += Vector3.new(2, 0, 2)
+	currentWall.CanCollide = false
+	local touchConnect
+	touchConnect = currentWall.Touched:Connect(function(hitPart)
+		if self.Game.Player == game.Players:GetPlayerFromCharacter(hitPart.Parent) then
+			touchConnect:Disconnect()
+			game:GetService("TweenService")
+				:Create(
+					Lighting:FindFirstChild("ColorCorrection"),
+					TweenInfo.new(0.5, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut, 0, true),
+					{ Brightness = 3 }
+				)
+				:Play()
+			currentWall.Size = defaltSize
+			local teleportWall = self.CorridorTeleports[math.random(#self.CorridorTeleports)]
+			self:CreateCorridorContent(self.Corridor, teleportWall, currentWall)
+			self.Game.Player.Character:MoveTo(teleportWall.Position)
+		end
 	end)
 end
 
@@ -124,20 +169,22 @@ function Stage:FinishAction(result)
 	end
 end
 
-function Stage:Init() 
-	print('start final stage')
+function Stage:Init()
+	print("start final stage")
 
 	local pivot, size = game:GetService("CollectionService"):GetTagged("PrevRoom")[1]:GetBoundingBox()
 	game:GetService("CollectionService"):GetTagged("PrevRoom")[1]:Destroy()
-	self.SpawnPivot = {pivot, size}
+	self.SpawnPivot = { pivot, size }
 
 	self:CreateCorridor()
 	self:Setup()
 	self:Action()
 
-	repeat wait() until self.IsReady
+	repeat
+		wait()
+	until self.IsReady
 
-	print('finish final stage')
+	print("finish final stage")
 end
 
 return Stage
